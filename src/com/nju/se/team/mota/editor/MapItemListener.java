687db041457006f5699f36513c52ff33dcacc906
linkeo <linkeo@sohu.com>
2013-09-11 11:41:04 +0800

    load levels(patch)

---
+++ src/com/nju/se/team/mota/editor/MapItemListener.java
@@ -0,0 +1,10 @@
+package com.nju.se.team.mota.editor;
+
+import java.util.Collection;
+
+import com.nju.se.team.mota.game.unit.Unit;
+
+public interface MapItemListener {
+	public void mapItemSelected(Unit u);
+	public void mapItemsUpdated(Collection<Unit> units);
+}
